3295ebdbd495e976f8f883d0a79816c8145dc779
vansh-frontend <vanshdhalor04>
2024-07-11 20:31:46 +0530

    first

--- login/homepage.php
+++ login/homepage.php
@@ -7,15 +7,17 @@ include("connect.php");
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
 </head>
+
 <body>
     <div style="text-align:center; padding:15%;">
-      <p  style="font-size:50px; font-weight:bold;">
-       Hello  <?php 
+        <p style="font-size:50px; font-weight:bold;">
+            Hello <?php 
        if(isset($_SESSION['email'])){
         $email=$_SESSION['email'];
         $query=mysqli_query($conn, "SELECT users.* FROM `users` WHERE users.email='$email'");
@@ -24,9 +26,10 @@ include("connect.php");
         }
        }
        ?>
-       :)
-      </p>
-      <a href="logout.php">Logout</a>
+            :)
+        </p>
+        <a href="logout.php">Logout</a>
     </div>
 </body>
+
 </html>
