acb12be2f6af2e10ddb49a09244aa3999b0dddc5
vansh-frontend <vanshdhalor04>
2024-07-12 11:47:54 +0530

    first

--- login/index.php
+++ login/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register & Login</title>
+    <title>Register &amp; Login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
